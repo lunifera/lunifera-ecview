e9db673cd9a9905991a53da3a7b437defc09851d
Edgar Mueller <edgar.m.mueller@gmail.com>
2011-09-12 16:11:32 +0200

    cleanup

--- org.eclipse.emf.emfstore.teamprovider.recording/src/org/eclipse/emf/emfstore/teamprovider/recording/AbstractVCSProvider.java
+++ org.eclipse.emf.emfstore.teamprovider.recording/src/org/eclipse/emf/emfstore/teamprovider/recording/AbstractVCSProvider.java
@@ -3,13 +3,10 @@ package org.eclipse.emf.emfstore.teamprovider.recording;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.commands.SerializationException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.EList;
@@ -18,11 +15,8 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.emfstore.common.model.util.ModelUtil;
 import org.eclipse.emf.emfstore.server.model.versioning.operations.AbstractOperation;
-import org.eclipse.emf.emfstore.teamprovider.recording.impl.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.team.core.history.IFileRevision;
 import org.xml.sax.InputSource;
 
 public abstract class AbstractVCSProvider<T,R> implements IVCSProvider<T,R> {
@@ -32,24 +26,6 @@ public abstract class AbstractVCSProvider<T,R> implements IVCSProvider<T,R> {
 	 */
 	public static final URI VIRTUAL_URI = URI.createURI("virtualUri");
 	
-	/*
-	// TODO: uitext descriptions
-	protected void merge(IResource resource, IProgressMonitor monitor)
-	throws InvocationTargetException, InterruptedException {
-				
-	
-		IRevision<T> headCommit = getHeadRevision(resource, monitor);
-		IRepository<R> repository = getRepository(resource.getProject());
-		IRevision<T> ancestorCommit = getAncestorRevision(rightCommit, headCommit, repository, monitor);
-		
-		String rightFile = getFile(repository, rightCommit, monitor);
-		String headFile = getFile(repository, headCommit, monitor);
-		String ancFile = getFile(repository, ancestorCommit, monitor);
-		
-		List<AbstractOperation> headOperations = getOperations(headFile);
-		List<AbstractOperation> rightOperations = getOperations(rightFile);
-	}*/
-
 	public List<AbstractOperation> getRightOperations(IResource resource, IProgressMonitor monitor) {
 		try {
 			IRevision<T> rightCommit = getRightRevision(resource, monitor);
