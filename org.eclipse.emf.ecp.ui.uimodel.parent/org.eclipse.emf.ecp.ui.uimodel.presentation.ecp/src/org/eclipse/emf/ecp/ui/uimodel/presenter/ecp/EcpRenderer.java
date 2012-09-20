a4eec688476141288477f855b84b4e38969380ae
Florian Pirchner <florian.pirchner@gmail.com>
2012-09-20 17:19:50 +0200

    Mavenized the projects

---
+++ org.eclipse.emf.ecp.ui.uimodel.parent/org.eclipse.emf.ecp.ui.uimodel.presentation.ecp/src/org/eclipse/emf/ecp/ui/uimodel/presenter/ecp/EcpRenderer.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Florian Pirchner
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Florian Pirchner - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.ecp.ui.uimodel.presenter.ecp;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecp.ui.uimodel.core.editparts.context.ContextException;
+import org.eclipse.emf.ecp.ui.uimodel.core.editparts.context.IViewContext;
+import org.eclipse.emf.ecp.ui.uimodel.core.editparts.presentation.IRenderer;
+
+/**
+ * A special implementation for ECP that offers convenience methods.
+ */
+public class EcpRenderer implements IRenderer {
+
+	public static final String UI_KIT_URI = "http://eclipse.org/emfclient/mekit";
+
+	@Override
+	public void render(IViewContext viewContext, Object rootLayout, Map<String, Object> options)
+		throws ContextException {
+
+	}
+
+}
--- org.eclipse.emf.ecp.ui.uimodel.presentation.ecp/src/org/eclipse/emf/ecp/ui/uimodel/presenter/ecp/internal/AbstractSWTWidgetPresenter.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.emf.ecp.ui.uimodel.presenter.ecp.internal;
-
-import org.eclipse.emf.ecp.ui.uimodel.core.editparts.presenter.IWidgetPresenter;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-
-public abstract class AbstractSWTWidgetPresenter implements IWidgetPresenter<Control, Composite> {
-
-}
