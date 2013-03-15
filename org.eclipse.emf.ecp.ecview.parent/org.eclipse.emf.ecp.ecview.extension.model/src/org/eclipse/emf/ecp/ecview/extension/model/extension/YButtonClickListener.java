3ce244e5f83efb61b6c1b34fc3c12c0c0ee0f0ee
Florian Pirchner <florian.pirchner@gmail.com>
2013-03-15 09:19:09 +0100

    Added YButtonClickListener

---
+++ org.eclipse.emf.ecp.ecview.parent/org.eclipse.emf.ecp.ecview.extension.model/src/org/eclipse/emf/ecp/ecview/extension/model/extension/YButtonClickListener.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.eclipse.emf.ecp.ecview.extension.model.extension;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>YButton Click Listener</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.emf.ecp.ecview.extension.model.extension.ExtensionModelPackage#getYButtonClickListener()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface YButtonClickListener {
+} // YButtonClickListener
