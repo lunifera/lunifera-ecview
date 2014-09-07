d81e6cf99906b54bf5609c51bafecca5ca1f94e6
Florian Pirchner <florian.pirchner@gmail.com>
2014-09-07 13:26:24 +0200

    Removed SWT stuff

--- org.lunifera.ecview.core.ui.presentation.ecp/src/org/eclipse/emf/ecp/ui/uimodel/presenter/ecp/internal/AbstractSWTWidgetPresenter.java
+++
@@ -1,8 +0,0 @@
-package org.lunifera.ecview.core.ui.presenter.ecp.internal;
-
-import org.lunifera.ecview.core.ui.core.editparts.presentation.IWidgetPresentation;
-import org.eclipse.swt.widgets.Control;
-
-public abstract class AbstractSWTWidgetPresenter implements IWidgetPresentation<Control> {
-
-}
